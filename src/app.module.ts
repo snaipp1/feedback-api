9248ee192621aba3242fe5e40c2c0a89688e2197
snaipp1 <snaipp1@yandex.by>
2021-08-31 20:11:50 +0300

    feat: add base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { RewiewModule } from './rewiew/rewiew.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TopPageModule, ProductModule, RewiewModule],
   controllers: [AppController],
   providers: [AppService],
 })
