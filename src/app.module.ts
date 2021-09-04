d2dd668f11a8a523ccc960d9d770fe36a61ebaaf
snaipp1 <snaipp1@yandex.by>
2021-09-04 21:51:56 +0300

    feat: add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { RewiewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, RewiewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    RewiewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
